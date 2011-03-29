90cb819d5e95178903bdba07dbd977df38e67dab
Jeff Zellman <jzellman@gmail.com>
2011-03-28 21:54:29 -0500

    1-2 exercise

---
+++ 1-2/hello.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+main() {
+  printf("hello world\a");
+  printf("hello world\b");
+  printf("hello world\c");
+  printf("hello world\d");
+  printf("hello world\e");
+  printf("hello world\f");
+  printf("hello world\g");
+  printf("hello world\h");
+  printf("hello world\i");
+  printf("hello world\j");
+  printf("hello world\k");
+  printf("hello world\l");
+  printf("hello world\m");
+  printf("hello world\n");
+  printf("hello world\o");
+  printf("hello world\p");
+  printf("hello world\q");
+  printf("hello world\r");
+  printf("hello world\s");
+  printf("hello world\t");
+  printf("hello world\u");
+  printf("hello world\v");
+  printf("hello world\w");
+  printf("hello world\x");
+  printf("hello world\y");
+  printf("hello world\z");
+}
